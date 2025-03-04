7a83007bceb3415b694bdfc3aec81939e074836f
key0406 <ohirakoki@gmail.com>
2025-03-04 15:01:13 +0000

    Database structure updated

--- app/src/main/java/com/example/staysafe/model/dao/UserDao.kt
+++ app/src/main/java/com/example/staysafe/model/dao/UserDao.kt
@@ -4,19 +4,31 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.example.staysafe.model.data.User
 
 @Dao
 interface UserDao {
+    //Create
     @Insert
     suspend fun insertUser(user: User)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(dummyUsers: List<User>)
-
+    //Get user
     @Query("SELECT * FROM users WHERE userID = :id")
     suspend fun getUserById(id: Long): User?
 
     @Query("SELECT * FROM users")
     suspend fun getAllUsers(): List<User>  // Ensure this matches the entity
+    /*update*/
+    @Update
+    suspend fun updateUser(user: User)
+    /*delete*/
+    @Query("DELETE FROM users WHERE userID = :id")
+    suspend fun deleteUserById(id: Long)
+    /*Searching user that attached to the location*/
+    @Query("SELECT COUNT(*) FROM users WHERE userID = :id")
+    suspend fun isUserExist(id: Long)
+
 }
--- app/src/main/java/com/example/staysafe/model/data/Location.kt
+++ app/src/main/java/com/example/staysafe/model/data/Location.kt
@@ -1,14 +1,29 @@
 package com.example.staysafe.model.data
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "locations")
+@Entity(
+    tableName = "locations",
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = ["userID"],
+            childColumns = ["userID"],
+            onDelete = ForeignKey.CASCADE,
+            deferred = true
+        )
+    ],
+    indices = [Index(value = ["userID"])] // Fix: Add index to foreign key
+)
 data class Location(
     @PrimaryKey(autoGenerate = true) val locationID: Long = 0,
     val locationName: String,
     val locationAddress: String,
     val locationPostcode: String,
     val locationLatitude: Double,
-    val locationLongitude: Double
+    val locationLongitude: Double,
+    val userID: Long
 )
