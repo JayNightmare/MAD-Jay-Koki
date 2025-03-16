99339359419d34c002176fe9e2c15fca3139d062
Jay <jordanbell321@gmail.com>
2025-03-16 17:35:09 +0000

    Removed unused files.

--- app/src/main/java/com/example/staysafe/nav/Screen.kt
+++ app/src/main/java/com/example/staysafe/nav/Screen.kt
@@ -2,7 +2,6 @@ package com.example.staysafe.nav
 
 sealed class Screen(val route:String) {
     object MapScreen: Screen("map")
-    object PeopleScreen: Screen("people")
     object LoginScreen: Screen("login")
     //For contact and call screen added later
 }
